90ca68cf1ca1b1d5e483f6fa7c41333e34372408
Luis Alejandro Hermida <luis.hn@zoho.com>
2020-04-03 11:05:51 -0600

    added projects

--- client/src/app/projects/react.js
+++ client/src/app/projects/react.js
@@ -3,19 +3,10 @@ export const reactProjects = [
     name: "Tetris React",
     image: "../../assets/nav-background.jpg",
     description: "Tetris game with levels and score. Made using React hooks.",
-    demoUrl: "",
+    demoUrl: "https://tetris-react-lahn.herokuapp.com/",
     codeUrl: "https://github.com/Luis-Hermida/Tetris-React",
-    downloadUrl: "",
-    technologies: [
-      "React",
-      "React",
-      "React",
-      "React",
-      "React",
-      "React",
-      "React",
-      "React"
-    ],
+    downloadUrl: "https://github.com/Luis-Hermida/Tetris-React",
+    technologies: ["React"],
     projectImages: [
       "../../assets/certificates/react.png",
       "../../assets/certificates/react_native.png"
